ebc866a0951899038edaba770fef75c6625f2427
XutaxKamay <xutaxkamay@gmail.com>
2019-06-06 21:54:39 +0200

    Fixed error on reading sources signatures

--- prohacktive_fetch.py
+++ prohacktive_fetch.py
@@ -13,7 +13,7 @@ colors.print_info("[-] ProHacktive fetching running...")
 srcsmanager = SourcesManager()
 if srcsmanager.fetch_all():
     colors.print_info("[-] Sources generated signatures:")
-    for source in srcsmanager.readSrcsSigs():
+    for source in srcsmanager.read_srcs_sigs():
         colors.print_info("     Source Name: %s -> %s" % (source["_id"], source["sig"]))
     colors.print_success("[x] ProHacktive fetching done!")
 else:
--- test.py
+++ test.py
@@ -5,10 +5,9 @@ import json
 runPath = os.path.dirname(os.path.realpath(__file__ + "../"))
 sys.path.append(runPath + "/lib/")
 
-import phdb
-import sourcesmanager
 import colors
+import sourcesmanager
 
 srcsmanager = sourcesmanager.SourcesManager()
-for source in srcsmanager.readSrcsSigs():
+for source in srcsmanager.read_srcs_sigs():
     colors.print_info("     Source Name: %s -> %s" % (source["_id"], source["sig"]))
